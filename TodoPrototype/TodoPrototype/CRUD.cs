6514afd41a1f18405a5a5596d83e6991ee7fb00c
Tim <tferris88@gmail.com>
2018-12-01 09:16:17 -0500

    started serializable class

--- TodoPrototype/TodoPrototype/CRUD.cs
+++ TodoPrototype/TodoPrototype/CRUD.cs
@@ -10,50 +10,6 @@ namespace TodoPrototype
 {
     class CRUD
     {
-        private const string Path = @"c:\C#Basics\Prototype\TodoPrototype\PrototypeFiles\Todos.txt";
-
-        public static string GetPath()
-        {
-            var rawPath = CRUD.Path;
-            var DS = System.IO.Path.DirectorySeparatorChar;
-            var path = rawPath.Replace('\\', DS);
-
-            return path;
-        }
-
-        public static string GetFile()
-        {
-            var Path = CRUD.GetPath();
-
-            if (!File.Exists(Path))
-            {
-                var newFile = File.Create(Path);
-                newFile.Close();
-            }
-
-            var contents = File.ReadAllText(Path);
-
-            return contents;
-        }
-
-        public static TodoPrototype.Task Read()
-        {
-            var content = GetFile();
-
-            return content;
-        }
-
-        public static TodoPrototype.Task Create()
-        {
-            Task CurrentTasks = CRUD.Read();
-            int ID;
-            var label = Helper.GetLabel();
-            var content = Helper.GetContent();
-
-            Task task = new Task(ID, label, content);
-
-            return task;
-        }
 
     }
 }
---
+++ TodoPrototype/TodoPrototype/TaskCollection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoPrototype
+{
+    class TaskCollection
+    {
+        private static TaskCollection taskCollection;
+        private Dictionary<int, Task> Tasks;
+        private BinaryFormatter Formatter;
+
+        public static TaskCollection Instance()
+        {
+            if (taskCollection == null)
+            {
+                taskCollection = new TaskCollection();
+            }
+
+            return taskCollection;
+        }
+
+        private TaskCollection()
+        {
+            this.Tasks = new Dictionary<int, Task>();
+            this.Formatter = new BinaryFormatter();
+        }
+    }
+}
